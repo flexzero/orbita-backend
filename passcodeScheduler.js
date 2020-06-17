81368f0309be36330cc861078dfe502490de9d33
flexzero <flexzero2016@gmail.com>
2020-06-17 16:02:09 +0530

    scheduling

--- passcodeScheduler.js
+++ passcodeScheduler.js
@@ -1,54 +1,31 @@
-const Bull = require("bull");
+const schedule = require('node-schedule');
 const mongoose = require("mongoose");
-const { ScheduledPasscodeModel } =  require("./models/model");
 
-require('dotenv').config();
+const { ScheduledPasscodeModel } = require("./models/model");
 
+// Setting mongo parameters, so that deprecated mongodb functions dont required
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-const { env: { MONGO_HOST, MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, MONGO_DB_NAME} } = process;
+let mongoConnectStrRoot = ``
 
-let mongoConnectStr = "";
+const GENERATE_PASSCODE_EVERY_MINUTE = "generate passcode every minute";
+const GENERATE_PASSCODE_EVERY_HOUR = "generate passcode every hour";
 
-MONGO_USERNAME === "" && MONGO_PASSWORD === "" ? mongoConnectStr = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`
-  : mongoConnectStr = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`;
+//Connection string for the orbita-project mongodb
+let mongoConnectStr = "mongodb+srv://orbita:crazyorbita@cluster1-7rnzb.mongodb.net/orbita-test?retryWrites=true&w=majority";
 
+//Conection string for the agenda task scheduler
+let agendaMongoConnectStr = "mongodb+srv://orbita:crazyorbita@cluster1-7rnzb.mongodb.net/agenda?retryWrites=true&w=majority";
+
+// This is the how the script will connect to project-orbita's mongodb
 mongoose.connect(mongoConnectStr, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on("error", (error) => console.log(error));
 mongoose.Promise = global.Promise;
 
-const passcodeQueue = new Bull('passcodeQueue');
 
-(async () => {
-    console.log(await ScheduledPasscodeModel.find({}));
-})();
-
-let main = async () => {
-    const job = await passcodeQueue.add({
-        command: 'GENERATE_PASSCODE',
-   }, {
-        repeat: {
-          every: 10000,
-        }
-   });
-}
-
-passcodeQueue.process(async job => {
-    try {
-        // TODO: check if there is an passcode scheduled to generated today
-        // check if it should be done in current hour
-        // if yes generate the passcode and mail the customer using node mailer
-    } catch (error) {
-        console.log(error);
-    }
+const job = schedule.scheduleJob('0 */1 * * * *', function() {
 });
-
-passcodeQueue.on('completed', job => {
-    console.log(`Job with id ${job.id} has been completed`);
-});
-
-main();
